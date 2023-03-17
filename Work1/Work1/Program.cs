ede8b4f07f9bd7abdd3782cd18c0807445e87980
3iN2 <111281649+3iN2@users.noreply.github.com>
2023-03-17 14:07:07 +0300

    Work_1
    Первое домашнее задание

---
+++ Work1/Work1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Код который выполняется в первую очередь
+        /// </summary>
+        /// <param name="args">параметры запуска</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет мир!!!"); //Задание 1
+            Console.ReadKey();
+
+
+
+            Console.Write("Привет "); //Задание 2
+            Console.Write("мир ");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
