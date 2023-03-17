37b45fecd0b4bee46c2dff23db7e974569243871
3iN2 <111281649+3iN2@users.noreply.github.com>
2023-03-17 16:50:30 +0300

    region
    проверил как работает #region

--- Work1/Work1/Program.cs
+++ Work1/Work1/Program.cs
@@ -14,15 +14,17 @@ namespace Work1
         /// <param name="args">параметры запуска</param>
         static void Main(string[] args)
         {
+            #region "Привет мир" через Write Line
             Console.WriteLine("Привет мир!!!"); //Задание 1
             Console.ReadKey();
+            #endregion
 
-
-
+            #region "Привет мир" через Write
             Console.Write("Привет "); //Задание 2
             Console.Write("мир ");
             Console.Write("!!!");
             Console.ReadKey();
+            #endregion
         }
     }
 }
